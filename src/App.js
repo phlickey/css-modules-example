1ccfc4f7cfc2acf4224b25f106ca1bfb9e6fc899
Philip Hickey <phlickey@gmail.com>
2020-11-20 21:14:11 +0000

    Demonstrate global modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./red.css";
 import scopedCssModule from "./blue.module.css";
+import "./green.module.css";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="red">This block is red</div>
       <div className="blue">This block isn't blue</div>
       <div className={scopedCssModule.blue}>This block is blue</div>
+      <div className="green">But this block is green</div>
       <pre>{JSON.stringify({ scopedCssModule }, null, 2)}</pre>
     </div>
   );
